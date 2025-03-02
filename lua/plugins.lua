0767827c204e8a6ce1bb41c9e3d37eda22be7e24
shadmeoli <shadcodes@gmail.com>
2025-03-03 00:08:15 +0300

    update: plugins addition for better navigation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,603 @@
 require("lazy").setup({
   -- File Explorer
   { "nvim-tree/nvim-tree.lua", config = function() require("nvim-tree").setup() end },
-})
 
+{
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('github-theme').setup({
+      -- ...
+    })
+    vim.cmd('colorscheme github_dark_tritanopia')
+  end,
+},
+
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = {
+      default = true,
+      color_icons = true,
+      strict = true,
+      override = {
+        zsh = {
+          icon = "",
+          color = "#428850",
+          cterm_color = "65",
+          name = "Zsh"
+        }
+      },
+      override_by_filename = {
+        [".gitignore"] = {
+          icon = "",
+          color = "#f1502f",
+          name = "Gitignore"
+        }
+      },
+      override_by_extension = {
+        ["log"] = {
+          icon = "",
+          color = "#81e043",
+          name = "Log"
+        }
+      },
+      override_by_operating_system = {
+        ["apple"] = {
+          icon = "",
+          color = "#A2AAAD",
+          cterm_color = "248",
+          name = "Apple",
+        },
+      },
+      variant = "dark",
+    }
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      local autopairs = require("nvim-autopairs")
+      
+      autopairs.setup({
+        check_ts = true,
+        ts_config = {
+          lua = { "string", "source" },
+          javascript = { "string", "template_string" },
+          typescript = { "string", "template_string" },
+          javascriptreact = { "string", "template_string" },
+          typescriptreact = { "string", "template_string" },
+        },
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        fast_wrap = {
+          map = "<M-e>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "Search",
+          highlight_grey = "Comment"
+        },
+        rules = {
+          {
+            "( ", " )",
+            filetype = { "typescript", "typescriptreact", "javascript", "javascriptreact" }
+          },
+          {
+            "{ ", " }",
+            filetype = { "typescript", "typescriptreact", "javascript", "javascriptreact", "lua" }
+          },
+        },
+      })
+
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      if cmp then
+        cmp.event:on(
+          "confirm_done",
+          cmp_autopairs.on_confirm_done()
+        )
+      end
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    priority = 1000,
+    lazy = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      
+      require("luasnip.loaders.from_vscode").lazy_load()
+      
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+          { name = "path" },
+        }),
+        formatting = {
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snippet]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
+      })
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" }
+        }
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" }
+        }, {
+          { name = "cmdline" }
+        })
+      })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "hrsh7th/nvim-cmp", lazy = false },
+      { "hrsh7th/cmp-nvim-lsp", lazy = false },
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "folke/neodev.nvim",
+    },
+    after = "nvim-cmp",  -- Add this line to load after nvim-cmp
+    config = function()
+      local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      
+      if has_cmp then
+        capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+      end
+
+      require("neodev").setup()
+      require("mason").setup({
+        ui = {
+          border = "rounded",
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+
+      require("mason-lspconfig").setup({
+        ensure_installed = { 
+          "lua_ls",
+          "eslint",  -- Changed from eslint_d to eslint
+          "pyright",
+          "gopls",
+          "cssls",
+          "html",
+          "emmet_ls",
+          "tailwindcss",
+        },
+        automatic_installation = true,
+      })
+
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp-nvim-lsp").default_capabilities()
+
+      vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+      vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set("n", "<space>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "<space>f", function() 
+            vim.lsp.buf.format { async = true } 
+          end, opts)
+        end,
+      })
+
+      -- Lua
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      })
+
+      -- TypeScript/JavaScript
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+      })
+
+      -- Python
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "workspace",
+              useLibraryCodeForTypes = true,
+              typeCheckingMode = "basic",
+            },
+          },
+        },
+      })
+
+      -- Go
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+              shadow = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+            usePlaceholders = true,
+            completeUnimported = true,
+            experimentalPostfixCompletions = true,
+          },
+        },
+      })
+
+      -- HTML
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+
+      -- CSS
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+
+      -- Emmet (HTML/CSS snippets)
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        filetypes = { 
+          "html", "typescriptreact", "javascriptreact", "css", 
+          "sass", "scss", "less", "vue" 
+        },
+      })
+
+      -- ESLint
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+      })
+
+      -- Tailwind CSS
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+    end,
+  },
+
+  -- Treesitter for better syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua", "vim", "vimdoc", "query",  -- Neovim
+          "javascript", "typescript", "tsx", "html", "css", -- Web
+          "python", "go", -- Backend
+          "json", "yaml", "toml", -- Config files
+          "markdown", "markdown_inline", -- Documentation
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      })
+    end,
+  },
+
+  -- Python specific tools
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")
+      
+      local dap = require("dap")
+      local dapui = require("dapui")
+      
+      dapui.setup()
+      
+      -- Automatically open/close dapui when debugging starts/ends
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+      
+      -- Key mappings for Python debugging
+      vim.keymap.set("n", "<F5>", dap.continue)
+      vim.keymap.set("n", "<F10>", dap.step_over)
+      vim.keymap.set("n", "<F11>", dap.step_into)
+      vim.keymap.set("n", "<F12>", dap.step_out)
+      vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<leader>B", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end)
+    end,
+  },
+
+  -- Go specific tools
+  {
+    "ray-x/go.nvim",
+    dependencies = {
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup({
+        -- Golang setup
+        go = "go",
+        goimport = "gopls",
+        fillstruct = "gopls",
+        gofmt = "gofumpt",
+        max_line_len = 120,
+        tag_transform = false,
+        test_template = "",
+        test_template_dir = "",
+        comment_placeholder = "",
+        verbose = false,
+        -- Linters and formatters
+        lsp_cfg = true,
+        lsp_on_attach = true,
+        lsp_keymaps = true,
+        lsp_codelens = true,
+        gopls_remote_auto = true,
+        gopls_cmd = nil,
+        dap_debug = true,
+        dap_debug_gui = true,
+      })
+      
+      -- Run gofmt on save
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          require("go.format").goimport()
+        end,
+        group = vim.api.nvim_create_augroup("GoFormat", {}),
+      })
+      
+      -- Go key mappings
+      vim.keymap.set("n", "<leader>gt", ":GoTest<CR>")
+      vim.keymap.set("n", "<leader>gtf", ":GoTestFunc<CR>")
+      vim.keymap.set("n", "<leader>gr", ":GoRun<CR>")
+      vim.keymap.set("n", "<leader>gi", ":GoImport<CR>")
+    end,
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()',
+  },
+
+  -- React/TypeScript tools
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = {
+      "typescript",
+      "typescriptreact",
+      "javascript",
+      "javascriptreact",
+    },
+    config = function()
+      require("typescript-tools").setup({
+        settings = {
+          tsserver_file_preferences = {
+            importModuleSpecifierPreference = "relative",
+            jsxAttributeCompletionStyle = "auto",
+            includeCompletionsForImportStatements = true,
+            includeCompletionsWithSnippetText = true,
+            includeAutomaticOptionalChainCompletions = true,
+            includeCompletionsWithClassMemberSnippets = true,
+            includeCompletionsWithInsertText = true,
+          },
+          -- Specify exposed actions
+          expose_as_code_action = {
+            "add_missing_imports",
+            "remove_unused",
+            "remove_unused_imports",
+            "organize_imports",
+            "fix_all",
+          },
+          -- Additional code actions through tsserver
+          tsserver_format_options = {
+            allowIncompleteCompletions = true,
+            allowRenameOfImportPath = true,
+          },
+        },
+      })
+      
+      -- Quick actions for React/TypeScript development
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+        callback = function()
+          vim.keymap.set("n", "<leader>oi", ":TSToolsOrganizeImports<CR>", { buffer = true })
+          vim.keymap.set("n", "<leader>ru", ":TSToolsRemoveUnused<CR>", { buffer = true })
+          vim.keymap.set("n", "<leader>fa", ":TSToolsFixAll<CR>", { buffer = true })
+          vim.keymap.set("n", "<leader>rf", ":TSToolsRenameFile<CR>", { buffer = true })
+        end,
+        group = vim.api.nvim_create_augroup("TypeScriptMappings", {}),
+      })
+    end,
+  },
+
+  -- Emmet
+  {
+    "mattn/emmet-vim",
+    ft = {
+      "html", "css", "javascript", "javascriptreact", 
+      "typescript", "typescriptreact", "vue"
+    },
+    init = function()
+      -- Enable emmet for JSX/TSX files
+      vim.g.user_emmet_settings = {
+        javascriptreact = {
+          extends = "jsx",
+        },
+        typescriptreact = {
+          extends = "tsx",
+        },
+      }
+      vim.g.user_emmet_mode = "a" -- Enable for all modes
+      vim.g.user_emmet_leader_key = ","
+    end,
+  },
+
+  -- Python code formatter
+  {
+    "psf/black",
+    ft = "python",
+    config = function()
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.py",
+        callback = function()
+          vim.cmd("Black")
+        end,
+        group = vim.api.nvim_create_augroup("PythonFormatting", {}),
+      })
+    end,
+  },
+
+  -- Python import sorter
+  {
+    "fisadev/vim-isort",
+    ft = "python",
+    config = function()
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.py",
+        callback = function()
+          vim.cmd("Isort")
+        end,
+        group = vim.api.nvim_create_augroup("PythonImportSorting", {}),
+      })
+    end,
+  },
+})
