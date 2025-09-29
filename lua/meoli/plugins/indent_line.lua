e80206dc58617b71e24e3a7d12a071d3ebd28e3a
shadmeoli <shadcodes@gmail.com>
2025-09-30 00:10:54 +0300

    fix: indentation clean up

--- lua/meoli/plugins/indent_line.lua
+++ lua/meoli/plugins/indent_line.lua
@@ -1,9 +1,31 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  event = { 'BufReadPost', 'BufNewFile' },
+  opts = {
+    indent = {
+      char = '│',
+      tab_char = '│',
+    },
+    scope = { enabled = false },
+    exclude = {
+      filetypes = {
+        'help',
+        'alpha',
+        'dashboard',
+        'neo-tree',
+        'Trouble',
+        'lazy',
+        'mason',
+        'notify',
+        'toggleterm',
+        'lazyterm',
+      },
+      buftypes = { 'terminal', 'nofile' },
+    },
   },
+  config = function(_, opts)
+    require('ibl').setup(opts)
+    vim.api.nvim_set_hl(0, 'IblIndent', { fg = '#3a3a3a', nocombine = true })
+  end,
 }
