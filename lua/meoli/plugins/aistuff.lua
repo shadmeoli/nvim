ae9b918e9f065750490dfa8afe7b2f37b6b7e5cb
shadmeoli <shadcodes@gmail.com>
2025-11-21 10:29:04 +0300

    fix: supamaven removal and theme update

--- lua/meoli/plugins/aistuff.lua
+++ lua/meoli/plugins/aistuff.lua
@@ -1,6 +1,9 @@
-return {
-  'supermaven-inc/supermaven-nvim',
-  config = function()
-    require('supermaven-nvim').setup {}
-  end,
-}
+return {}
+
+--
+-- return {
+--   'supermaven-inc/supermaven-nvim',
+--   config = function()
+--     require('supermaven-nvim').setup {}
+--   end,
+-- }
